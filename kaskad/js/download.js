9b35fc4bc2f40575127ecb32f6259fb2a6ef8202
MelkovDmitry <saim.asutp@gmail.com>
2024-07-03 11:03:46 +0500

    add preloader fix

--- kaskad/js/download.js
+++ kaskad/js/download.js
@@ -2,21 +2,23 @@ const iframeMenu = document.getElementById('frame_menu');
 const iframeData = document.getElementById('frame_data');
 const spiner = document.querySelector('.overlay');
 
-// Дождитесь полной загрузки iframe
-iframeMenu.onload = function () {
+iframeMenu.onload = () => {
   // Получите доступ к документу внутри iframe
   let iframeDocument = iframeMenu.contentDocument || iframeMenu.contentWindow.document;
-  // Найдите элемент по id внутри iframe
   let elementInsideIframe = iframeDocument.querySelectorAll('.page-sidebar__link');
 
   for (let i = 0; i < elementInsideIframe.length; i++) {
     const element = elementInsideIframe[i];
     element.addEventListener('click', (e) => {
+      iframeData.style.backgroundColor = 'white';
+      iframeData.style.visibility = 'hidden';
       spiner.classList.add('active');
     });
   }
 };
 
-iframeData.onload = function () {
+iframeData.onload = () => {
+  iframeData.style.backgroundColor = '';
+  iframeData.style.visibility = 'visible';
   spiner.classList.remove('active');
 };
